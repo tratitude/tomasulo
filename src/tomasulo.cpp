9e45f2f00b4ba53df533d9fa9746b42c09352a37
tratitude <tratitude@gmail.com>
2019-06-25 02:22:49 +0800

    sample successed

--- src/tomasulo.cpp
+++ src/tomasulo.cpp
@@ -44,6 +44,7 @@ typedef struct{
     int Qk; // if not used, set as -1
     int cycle;  // how many cycles needed, init as -1
     Instruction_t *ins;
+    bool last_writeback;
 } ReservationStation_t;
 
 typedef struct{
@@ -55,7 +56,7 @@ typedef struct{
 #define FR_N 16 // float point register number
 #define IR_N 32 // integer register number
 #define LD_C 2  // ld cycle
-#define SD_C 2  // sd cycle
+#define SD_C 1  // sd cycle
 #define ADD_C 2 // add cycle
 #define SUB_C 2 // sub cycle
 #define MUL_C 10 // mul cycle
@@ -67,73 +68,8 @@ vector<Instruction_t> Instruction;
 map<uint64_t, int> Memory;
 map<string, int> Cycle{{"L.D", LD_C}, {"S.D", SD_C}, {"ADD.D", ADD_C}, 
     {"SUB.D", SUB_C}, {"MUL.D", MUL_C}, {"DIV.D", DIV_C}};
-//set<pair<string, int>> OpTable;  // use to translate between string and int
-//set<pair<string, int>> RegTable;
-//map<string, int> RegValue;  // use to get register value from string
 int Clock = 0;
 int PC = 0;
-/*
-void init_regtable()
-{
-    int froffset = 100;
-    int iroffset = 200;
-    for(int i = 0; i < FR_N; i++)
-    {
-        string regstr("F");
-        int regnum = i*2;
-        regstr += to_string(regnum);
-        RegTable.insert(make_pair(regstr, froffset + regnum));
-    }
-    for (int i = 0; i < IR_N; i++)
-    {
-        string regstr("R");
-        int regnum = i;
-        regstr += to_string(regnum);
-        RegTable.insert(make_pair(regstr, iroffset + regnum));
-    }
-}
-int reg_encode(string regstr)
-{
-    for(auto it = RegTable.begin(); it != RegTable.end(); it++)
-    {
-        if (!regstr.compare(it->first))
-            return it->second;
-    }
-}
-string reg_decode(int reg)
-{
-    for (auto it = RegTable.begin(); it != RegTable.end(); it++)
-    {
-        if (it->second == reg)
-            return it->first;
-    }
-}
-void init_optable()
-{
-    OpTable.insert(make_pair("L.D", LD));
-    OpTable.insert(make_pair("S.D", SD));
-    OpTable.insert(make_pair("ADD.D", ADD));
-    OpTable.insert(make_pair("SUB.D", SUB));
-    OpTable.insert(make_pair("MUL.D", MUL));
-    OpTable.insert(make_pair("DIV.D", DIV));
-}
-int op_encode(string opstr)
-{
-    for (auto it = RegTable.begin(); it != RegTable.end(); it++)
-    {
-        if (!opstr.compare(it->first))
-            return it->second;
-    }
-}
-string op_decode(int op)
-{
-    for (auto it = RegTable.begin(); it != RegTable.end(); it++)
-    {
-        if (it->second == op)
-            return it->first;
-    }
-}
-*/
 void init_Register()
 {
     for (int i = 0; i < FR_N; i++)
@@ -143,7 +79,7 @@ void init_Register()
         regstr += to_string(regnum);
         Register_t reg;
         reg.fu = -1;
-        reg.value = regnum;
+        reg.value = 1;
         Register.insert(make_pair(regstr, reg));
     }
     for (int i = 0; i < IR_N; i++)
@@ -163,7 +99,7 @@ void init_Register()
 }
 /*
 ld rd, offset(rt)
-sd rd, offset(rt)
+sd rt, offset(rd)
 add rd, rs, rt
  */
 void init_Instruction(string filename)
@@ -205,6 +141,7 @@ void init_Instruction(string filename)
         }
         
         in.opcode = opstr; in.rd = rdstr; in.rs= rsstr; in.rt = rtstr;
+        in.Issue = -1; in.Execution = -1; in.Write = -1;
         Instruction.push_back(in);
     }
     ifile.close();
@@ -220,34 +157,41 @@ void print(string filename)
     ofile << setw(30) << "Instruction:" << setw(10) << "Issue" << setw(15) << "Exec_comp" << setw(15) << "Write_back" << endl;
     // print Instruction
     for(auto &it :Instruction){
-        ofile << setw(7) << it.opcode << setw(5) << it.rd << setw(5) << it.rs << setw(13) << it.rt
+        if(it.opcode == "L.D")
+            ofile << setw(7) << it.opcode << setw(5) << it.rd << setw(5) << it.offset << setw(13) << it.rt
               << setw(10) << it.Issue << setw(15) << it.Execution << setw(15) << it.Write << endl;
+        else if(it.opcode == "S.D")
+            ofile << setw(7) << it.opcode << setw(5) << it.rt << setw(5) << it.offset << setw(13) << it.rd
+              << setw(10) << it.Issue << setw(15) << it.Execution << setw(15) << it.Write << endl;
+        else
+            ofile << setw(7) << it.opcode << setw(5) << it.rd << setw(5) << it.rs << setw(13) << it.rt
+                << setw(10) << it.Issue << setw(15) << it.Execution << setw(15) << it.Write << endl;
     }
     // print function unit
     ofile << endl;
-    ofile << setw(7) << "Time" << setw(7) << "Name" << setw(7) << "Busy" << setw(7) << "Op" << setw(7) << "Vj" << setw(7) << "Vk" << setw(7) << "Qj" << setw(7) << "Qk" << endl;
-    for(int i = Adder0; i <= Adder2; i++){
-        ReservationStation_t &res = ReservationStation[i];
-        ofile << setw(7) << res.cycle << setw(7) << "ADD" + to_string(i-Adder0+1) << setw(7) << to_string(ReservationStation[i].busy) << setw(7) << 
-            res.opcode << setw(10) << res.Vj << setw(10) << res.Vk << setw(7) << res.Qj << setw(7) << res.Qk << endl;
-    }
-    ofile << endl;
-    for(int i = Multiplier0; i <= Multiplier1; i++){
-        ReservationStation_t &res = ReservationStation[i];
-        ofile << setw(7) << res.cycle << setw(7) << "MUL" + to_string(i-Multiplier0+1) << setw(7) << to_string(ReservationStation[i].busy) << setw(7) << 
-            res.opcode << setw(10) << res.Vj << setw(10) << res.Vk << setw(7) << res.Qj << setw(7) << res.Qk << endl;
-    }
-    ofile << endl;
+    ofile << setw(7) << "Time" << setw(7) << "Name" << setw(7) << "Busy" << setw(7) << "Op" << setw(10) << "Vj" << setw(10) << "Vk" << setw(10) << "Qj" << setw(10) << "Qk" << endl;
     for(int i = LoadBuffer0; i <= LoadBuffer1; i++){
         ReservationStation_t &res = ReservationStation[i];
         ofile << setw(7) << res.cycle << setw(7) << "LOAD" + to_string(i-LoadBuffer0+1) << setw(7) << to_string(ReservationStation[i].busy) << setw(7) << 
-            res.opcode << setw(10) << res.Vj << setw(10) << res.Vk << setw(7) << res.Qj << setw(7) << res.Qk << endl;
+            res.opcode << setw(10) << res.Vj << setw(10) << res.Vk << setw(10) << res.Qj << setw(10) << res.Qk << endl;
     }
     ofile << endl;
     for(int i = StoreBuffer0; i <= StoreBuffer1; i++){
         ReservationStation_t &res = ReservationStation[i];
         ofile << setw(7) << res.cycle << setw(7) << "STORE" + to_string(i-StoreBuffer0+1) << setw(7) << to_string(ReservationStation[i].busy) << setw(7) << 
-            res.opcode << setw(10) << res.Vj << setw(10) << res.Vk << setw(7) << res.Qj << setw(7) << res.Qk << endl;
+            res.opcode << setw(10) << res.Vj << setw(10) << res.Vk << setw(10) << res.Qj << setw(10) << res.Qk << endl;
+    }
+    ofile << endl;
+    for(int i = Adder0; i <= Adder2; i++){
+        ReservationStation_t &res = ReservationStation[i];
+        ofile << setw(7) << res.cycle << setw(7) << "ADD" + to_string(i-Adder0+1) << setw(7) << to_string(ReservationStation[i].busy) << setw(7) << 
+            res.opcode << setw(10) << res.Vj << setw(10) << res.Vk << setw(10) << res.Qj << setw(10) << res.Qk << endl;
+    }
+    ofile << endl;
+    for(int i = Multiplier0; i <= Multiplier1; i++){
+        ReservationStation_t &res = ReservationStation[i];
+        ofile << setw(7) << res.cycle << setw(7) << "MUL" + to_string(i-Multiplier0+1) << setw(7) << to_string(ReservationStation[i].busy) << setw(7) << 
+            res.opcode << setw(10) << res.Vj << setw(10) << res.Vk << setw(10) << res.Qj << setw(10) << res.Qk << endl;
     }
     ofile << endl << "Register Result Status" << endl;
     for(auto &it :Register){
@@ -295,21 +239,17 @@ void print(string filename)
     ofile << endl;
     for(auto &it :Memory)
         ofile << setw(10) << it.second;
+    ofile << endl << "-----------------------------------------------------" << endl;
     ofile.close();
 }
 void init_ReservationStation(int fu)
 {
-    ReservationStation[fu].busy = false;
-    ReservationStation[fu].cycle = -1;
-    ReservationStation[fu].Qi = -1;
-    ReservationStation[fu].Qj = -1;
-    ReservationStation[fu].Qk = -1;
-    ReservationStation[fu].ins = NULL;
+    ReservationStation[fu] = {false, "", -1, -1, -1, -1, -1, -1, -1, NULL, true};
 }
 void init_ReservationStation()
 {
     for(int i = 0; i < FU_N; i++){
-        init_ReservationStation(i);
+        ReservationStation[i] = {false, "", -1, -1, -1, -1, -1, -1, -1, NULL, false};
     }
 }
 float execute_operation(string op, float rs, float rt)
@@ -331,24 +271,38 @@ bool WriteResult()
         ReservationStation_t &res = ReservationStation[i];
         // check
         if(res.ins == NULL)
-            break;
-        Register_t &rdreg = Register[res.ins->rd];
-        if(res.cycle == 0 && res.Qi >= 0 && rdreg.fu != i){
-            // write result
-            if(i == LoadBuffer0 || i == LoadBuffer1){
-                int value = 1;
-                auto it = Memory.find((uint64_t)res.Vi);
-                if (it != Memory.end())
-                    value = it->second;
-                rdreg.value = value;
-            }
-            else if(i == StoreBuffer0 || i == StoreBuffer1)
+            continue;
+        if(res.opcode == "S.D"){
+            Register_t &rdreg = Register[res.ins->rt];
+            if(res.cycle == 0 && rdreg.fu != res.Qk){
+                // write result
                 Memory[(uint64_t)res.Vi] = rdreg.value;
-            else
-                rdreg.value = res.Vi;
-            rdreg.fu = -1;
-            init_ReservationStation(i);
-            wrote = true;
+                res.ins->Write = Clock;
+                if(rdreg.fu == i)
+                    rdreg.fu = -1;
+                init_ReservationStation(i);
+                wrote = true;
+            }
+        }
+        else{
+            Register_t &rdreg = Register[res.ins->rd];
+            if(res.cycle == 0 && rdreg.fu != res.Qi){
+                // write result
+                if(i == LoadBuffer0 || i == LoadBuffer1){
+                    int value = 1;
+                    auto it = Memory.find((uint64_t)res.Vi);
+                    if (it != Memory.end())
+                        value = it->second;
+                    rdreg.value = value;
+                }
+                else
+                    rdreg.value = res.Vi;
+                res.ins->Write = Clock;
+                if (rdreg.fu == i)
+                    rdreg.fu = -1;
+                init_ReservationStation(i);
+                wrote = true;
+            }
         }
     }
     return !wrote;
@@ -357,14 +311,23 @@ bool WriteResult()
 bool Execute()
 {
     bool executed = false;  // if executed set as true
-    // ld or sd
-    for(int i = LoadBuffer0; i <= StoreBuffer1; i++){
+    // ld
+    for(int i = LoadBuffer0; i <= LoadBuffer1; i++){
         ReservationStation_t &res = ReservationStation[i];
         if(res.ins == NULL)
-            break;
+            continue;
         switch(res.cycle){
+            // RAW hazard
+            case -1:
+                if(res.Qk >=0 && Register[res.ins->rt].fu != res.Qk){
+                    res.Vk = Register[res.ins->rt].value;
+                    res.Qk = -1;
+                    res.cycle = LD_C;
+                    executed = true;
+                }
+                break;
             // put rt value into res
-            case 2:
+            case LD_C:
                 res.Vk = Register[res.ins->rt].value;
                 --res.cycle;
                 executed = true;
@@ -378,19 +341,34 @@ bool Execute()
                 break;
         }
     }
+    // sd
+    for(int i = StoreBuffer0; i <= StoreBuffer1; i++){
+        ReservationStation_t &res = ReservationStation[i];
+        if(res.ins == NULL)
+            continue;
+        switch(res.cycle){
+            // execute
+            case SD_C:
+                res.Vk = Register[res.ins->rd].value;
+                res.Vi = execute_operation(res.opcode, res.Vj, res.Vk);
+                --res.cycle;
+                res.ins->Execution = Clock;
+                executed = true;
+                break;
+        }
+    }
     for(int i = Adder0; i <= Multiplier1; i++){
         ReservationStation_t &res = ReservationStation[i];
         if (res.ins == NULL)
-            break;
+            continue;
         switch(res.cycle){
             // rs or rt is not ready
             case -1:
-                if(res.Qj >= 0 && Register[res.ins->rs].fu != i){
+                if(res.Qj >= 0 && Register[res.ins->rs].fu != res.Qj){
                     res.Vj = Register[res.ins->rs].value;
                     res.Qj = -1;
                 }
-                    
-                if(res.Qk >= 0 && Register[res.ins->rt].fu != i){
+                if(res.Qk >= 0 && Register[res.ins->rt].fu != res.Qk){
                     res.Vk = Register[res.ins->rt].value;
                     res.Qk = -1;
                 }
@@ -399,9 +377,10 @@ bool Execute()
                     executed = true;
                 }
                 break;
-            case 0:
+            case 1:
                 res.Vi = execute_operation(res.opcode, res.Vj, res.Vk);
                 res.ins->Execution = Clock;
+                --res.cycle;
                 executed = true;
                 break;
             default:
@@ -421,21 +400,31 @@ void store_ins_to_res(int fu)
     res.busy = true;
     res.opcode = op;
     res.ins = &ins;
-    if (op == "L.D" || op == "S.D")
-    {
+    if (op == "L.D"){
         res.Vj = ins.offset; // ld or sd
         res.cycle = LD_C;    // ld and sd have same cycle
-        // find rd whether has WAW hazard
+        // find rd whether has WAW hazard, but not consider sd's hazard
         if (Register[ins.rd].fu >= 0) // hazard occur
             res.Qi = Register[ins.rd].fu;
-        // find rt whether has RAW hazard
-        if (Register[ins.rt].fu >= 0) // hazard occur
+        // find rt whether has RAW hazard, but not consider sd's hazard
+        if (Register[ins.rt].fu >= 0){  // hazard occur
             res.Qk = Register[ins.rt].fu;
-        else
-            res.Vk = Register[ins.rt].value;
+            res.cycle = -1;
+        }
     }
-    else
-    {
+    else if (op == "S.D"){
+        res.Vj = ins.offset; // ld or sd
+        res.cycle = SD_C;    // ld and sd have same cycle
+        // find rd whether has WAW hazard, but not consider sd's hazard
+        /*
+        if (Register[ins.rd].fu >= 0) // hazard occur
+            res.Qi = Register[ins.rd].fu;
+        */
+        // find rt whether has RAW hazard, but not consider sd's hazard
+        if (Register[ins.rt].fu >= 0)  // hazard occur
+            res.Qk = Register[ins.rt].fu;
+    }
+    else{
         /* map find method
         // find rs whether has RAW hazard
         auto rs = Register.find(ins.rs);
@@ -454,69 +443,79 @@ void store_ins_to_res(int fu)
         else
             res.Vk = rt->second.fu;
         */
+       res.cycle = Cycle[res.opcode];
         // find rd whether has WAW hazard
         if (Register[ins.rd].fu >= 0) // hazard occur
             res.Qi = Register[ins.rd].fu;
         // find rs whether has RAW hazard
-        if (Register[ins.rs].fu >= 0) // hazard occur
+        if (Register[ins.rs].fu >= 0){ // hazard occur
             res.Qj = Register[ins.rs].fu;
+            res.cycle = -1;
+        }
         else
             res.Vj = Register[ins.rs].value;
         // find rt whether has RAW hazard
-        if (Register[ins.rt].fu >= 0) // hazard occur
+        if (Register[ins.rt].fu >= 0){ // hazard occur
             res.Qk = Register[ins.rt].fu;
+            res.cycle = -1;
+        }
         else
             res.Vk = Register[ins.rt].value;
     }
     // update register function unit
-    Register[ins.rd].fu = fu;
+    if(ins.opcode != "S.D")
+        Register[ins.rd].fu = fu;
+    ++PC;
 }
 // if nothing to issue, return true
 bool Issue()
 {
+    if(Instruction.size() <= PC)
+        return true;
     bool issued = true;
     if(Instruction[PC].opcode == "L.D"){
-        if(!ReservationStation[LoadBuffer0].busy)
+        if(!ReservationStation[LoadBuffer0].busy && !ReservationStation[LoadBuffer0].last_writeback)
             store_ins_to_res(LoadBuffer0);
-        else if(!ReservationStation[LoadBuffer1].busy)
+        else if (!ReservationStation[LoadBuffer1].busy && !ReservationStation[LoadBuffer1].last_writeback)
             store_ins_to_res(LoadBuffer1);
         else
             issued = false;
     }
     else if(Instruction[PC].opcode == "S.D"){
-        if(!ReservationStation[StoreBuffer0].busy)
+        if (!ReservationStation[StoreBuffer0].busy && !ReservationStation[StoreBuffer0].last_writeback)
             store_ins_to_res(StoreBuffer0);
-        else if (!ReservationStation[StoreBuffer1].busy)
+        else if (!ReservationStation[StoreBuffer1].busy && !ReservationStation[StoreBuffer1].last_writeback)
             store_ins_to_res(StoreBuffer1);
         else
             issued = false;
     }
-    else if(Instruction[PC].opcode == "ADD.D"){
-        if(!ReservationStation[Adder0].busy)
+    else if(Instruction[PC].opcode == "ADD.D" || Instruction[PC].opcode == "SUB.D"){
+        if (!ReservationStation[Adder0].busy && !ReservationStation[Adder0].last_writeback)
             store_ins_to_res(Adder0);
-        else if (!ReservationStation[Adder1].busy)
+        else if (!ReservationStation[Adder1].busy && !ReservationStation[Adder1].last_writeback)
             store_ins_to_res(Adder1);
-        else if (!ReservationStation[Adder2].busy)
+        else if (!ReservationStation[Adder2].busy && !ReservationStation[Adder2].last_writeback)
             store_ins_to_res(Adder2);
         else
             issued = false;
     }
-    else if(Instruction[PC].opcode == "MUL.D"){
-        if(!ReservationStation[Multiplier0].busy)
+    else if(Instruction[PC].opcode == "MUL.D" || Instruction[PC].opcode == "DIV.D"){
+        if (!ReservationStation[Multiplier0].busy && !ReservationStation[Multiplier0].last_writeback)
             store_ins_to_res(Multiplier0);
-        else if (!ReservationStation[Multiplier1].busy)
+        else if (!ReservationStation[Multiplier1].busy && !ReservationStation[Multiplier1].last_writeback)
             store_ins_to_res(Multiplier1);
         else
             issued = false;
     }
     return !issued;
 }
-int main()
+int main(int argc, char *argv[])
 {
     //初始化各個結構
     //init_regtable();
     //init_optable();
-    string inputfile = ".\\doc\\test2";
+    string filename(argv[1]);
+    string inputfile = ".\\doc\\" + filename;
     string outputfile = inputfile + "_output.txt";
     init_Instruction(inputfile);
     init_Register();
@@ -524,14 +523,16 @@ int main()
     //何時離開主迴圈? WriteResult()、Execute()、Issue()皆無推進任何一個執令，或是struct Instruction中的Issue、Execution、Write皆非原初始值。
     while (1)
     {
+        ++Clock;
         bool finished = true;
         finished &= WriteResult(); //執行已完成Execution步驟的指令
         finished &= Execute();     //執行已完成Issue步驟的指令，並在維護每個正在本步驟的指令
         finished &= Issue();       //issue下一個未被issue的指令
+        for(int i = LoadBuffer0; i <= Multiplier1; i++)
+            ReservationStation[i].last_writeback = false;
         if(finished)
             break;
         print(outputfile);
-        Clock++;
     }
     
     return 0;
